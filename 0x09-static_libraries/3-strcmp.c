c5c719bcca2f49470268d6d0b056422092383f7c
unknown <adejokeadebobola@gmail.com>
2023-09-06 22:47:15 +0100

    commit all at once

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcmp-compare string values
+ * @s1: inpute value
+ * @s2:input value
+ * 
+ * Return:s1[i]-s2[i]
+ */
+int_strcmp(char *s1, char *s2)
+{
+	int i;
+	i=0;
+	while(s1[1] !='\0' && s2[i] !='\0')
+}
